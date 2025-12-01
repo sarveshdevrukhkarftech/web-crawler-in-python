2856124d5e512ba762c5f512974addc84b953ae8
sarveshdevrukhkarftech <sarveshdevrukhkar@fafadiatech.com>
2025-12-01 16:52:09 +0530

    Added new validation suggested by Sidharth sir.

--- crawler.py
+++ crawler.py
@@ -7,13 +7,14 @@ URL = "https://www.bbc.com"
 def fetch_page(url):
     try:
         response = requests.get(url, timeout=5)
-        response.raise_for_status()
-        print("Step 1 - Fetching Data Successful!")
+        # response.raise_for_status()
+        # print("Step 1 - Fetching Data Successful!")
+        print("1--------------------")
         return response
-    except requests.exceptions.Timeout:
-        print("Request timed out")
-    except requests.exceptions.HTTPError as error:
-        print(f"HTTP error: {error}")
+    # except requests.exceptions.Timeout:
+    #     print("Request timed out")
+    # except requests.exceptions.HTTPError as error:
+    #     print(f"HTTP error: {error}")
     except requests.exceptions.RequestException as error:
         print(f"Error during request: {error}")
 
@@ -21,7 +22,8 @@ def fetch_page(url):
 def parse_page(res):
     # Creating soup to extract data.
     parsed_data = BeautifulSoup(res.text, "html.parser")  # Parsing Soup Data
-    print("Step 2 - Parsing Data Successful!")
+    # print("Step 2 - Parsing Data Successful!")
+    print("2--------------------")
     return parsed_data
 
 
@@ -30,19 +32,23 @@ def parse_next_urls(parsed_data):
     all_link_tags = parsed_data.find_all("a")
     for tag in all_link_tags:
         link = tag.get("href")
+        if link is None:
+            continue
+        print(link)
 
         # Checking URL is valid or not.
-        if link.startswith("#"):
-            continue
-        if link.startswith("https://help.bbc.com/hc/"):
-            continue
-        if link.startswith("https"):
-            next_urls.append(link)
+        avoid_list = ["#", "mailto:"]
+        for item in avoid_list:
+            if link and link.find(item) != -1:
+                print("Avoided URL: ", link)
+                continue
+            elif link.startswith("https"):
+                next_urls.append(link)
+            elif link.startswith("/"):
+                next_urls.append(URL + link)
 
-        if link.startswith("/"):
-            next_urls.append(URL + link)
-
-    print("Step 3 - Parsing Next URLs Data Successful!")
+    # print("Step 3 - Parsing Next URLs Data Successful!")
+    print("3--------------------")
     return next_urls
 
 
@@ -50,7 +56,7 @@ frontier = ["https://www.bbc.com"]
 visited = {}
 
 while len(frontier) > 0:
-    current_url = frontier.pop()  # Removing URL from the Frontier List (DFS)
+    current_url = frontier.pop(0)  # Removing URL from the Frontier List (DFS)
     if current_url in visited:
         continue
     res = fetch_page(current_url)
